2dae6a62c7e45bf5fcc7f8fcb151ba641903c84f
Flac222 <sntsfdl02@gmail.com>
2026-05-20 01:28:14 -0300

    Fix project configuration and update gitignore

---
+++ app/src/main/java/com/uade/alltabs/core/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.uade.alltabs.core.di
+
+import com.uade.alltabs.data.repository.TabRepositoryImpl
+import com.uade.alltabs.domain.repository.TabRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindTabRepository(
+        tabRepositoryImpl: TabRepositoryImpl
+    ): TabRepository
+}
